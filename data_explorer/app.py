70820f06f24c8493318e569f308be0d5359aa83f
Josh Bishop <josh.bishop@refeyn.com>
2025-04-20 12:06:25 +0100

    Add support for simple arithmetic

--- data_explorer/app.py
+++ data_explorer/app.py
@@ -1,6 +1,7 @@
+import dataclasses
 import multiprocessing
 import sys
-from typing import Final, List, Optional, Sequence
+from typing import Callable, Final, List, Optional, Sequence
 
 import numpy as np
 import pyqtgraph as pg  # type: ignore[import-untyped]
@@ -14,13 +15,122 @@ BORDER_COLOUR: Final[str] = "y"
 COLOURMAPS: Final[list[str]] = ["gray", "viridis", "plasma", "inferno", "magma"]
 
 
+@dataclasses.dataclass
+class SimpleOperation:
+    description: str
+    operator: Optional[str]
+    calculation: Callable[[np.ndarray, np.ndarray], np.ndarray]
+
+
+SIMPLE_OPERATIONS: Final[list[SimpleOperation]] = [
+    SimpleOperation("Difference", "-", lambda a, b: a - b),
+    SimpleOperation("Division", "/", lambda a, b: a / b),
+]
+
+
+class OperationDock(widgets.QDockWidget):
+    """
+    A fixed dock for creating new derived arrays via arithmetic operations
+    """
+
+    def __init__(self, parent_app: "ArrayViewerApp") -> None:
+        super().__init__("Operations")
+        self.parent_app = parent_app
+        self.setFeatures(
+            widgets.QDockWidget.DockWidgetFeature.DockWidgetMovable
+        )  # No closing, no floating
+
+        self._init_ui()
+
+    def _init_ui(self) -> None:
+        self.main_widget = widgets.QWidget()
+        self.setWidget(self.main_widget)
+        layout = widgets.QVBoxLayout(self.main_widget)
+        # initial buttons panel
+        self.buttons_panel = widgets.QWidget()
+        btn_layout = widgets.QVBoxLayout(self.buttons_panel)
+        for operation in SIMPLE_OPERATIONS:
+            btn = widgets.QPushButton(
+                f"Create {operation.description.lower()} operation"
+            )
+            btn.clicked.connect(
+                lambda _checked, operation=operation: self._show_form(operation)
+            )
+            btn_layout.addWidget(btn)
+        layout.addWidget(self.buttons_panel)
+
+        # form panel hidden until needed
+        self.form_panel = widgets.QWidget()
+        form_layout = widgets.QGridLayout(self.form_panel)
+        self.operator_desc_label = widgets.QLabel("")
+        self.operator_op_label = widgets.QLabel("")
+
+        self.combo_a = widgets.QComboBox()
+        self.combo_b = widgets.QComboBox()
+        form_layout.addWidget(self.operator_desc_label, 0, 0)
+        form_layout.addWidget(self.combo_a, 0, 1)
+        form_layout.addWidget(self.operator_op_label, 0, 2)
+        form_layout.addWidget(self.combo_b, 0, 3)
+        self.create_btn = widgets.QPushButton("Create")
+        self.cancel_btn = widgets.QPushButton("Cancel")
+        form_layout.addWidget(self.create_btn, 1, 0)
+        form_layout.addWidget(self.cancel_btn, 1, 3)
+        layout.addWidget(self.form_panel)
+        self.form_panel.hide()
+
+        self.cancel_btn.clicked.connect(self._reset)
+        self.create_btn.clicked.connect(self._create)
+
+    def _show_form(self, operation: SimpleOperation) -> None:
+        # populate dropdowns with current dock titles
+        titles = [dock.get_title() for dock in self.parent_app.get_original_docks()]
+        self.combo_a.clear()
+        self.combo_a.addItems(titles)
+        self.combo_b.clear()
+        self.combo_b.addItems(titles)
+        self.operator_desc_label.setText(f"Create {operation.description.lower()}:")
+        if operation.operator is not None:
+            self.operator_op_label.setText(operation.operator)
+        self.current_op = operation
+        self.buttons_panel.hide()
+        self.form_panel.show()
+
+    def _reset(self) -> None:
+        self.form_panel.hide()
+        self.buttons_panel.show()
+
+    def _create(self) -> None:
+        a_title = self.combo_a.currentText()
+        b_title = self.combo_b.currentText()
+        # find arrays
+        a_arr = next(
+            d.get_array()
+            for d in self.parent_app.get_original_docks()
+            if d.get_title() == a_title
+        )
+        b_arr = next(
+            d.get_array()
+            for d in self.parent_app.get_original_docks()
+            if d.get_title() == b_title
+        )
+
+        self.parent_app._add_array(
+            array=self.current_op.calculation(a_arr, b_arr),
+            title=f"{a_title} {self.current_op.operator} {b_title}",
+            can_delete=True,
+        )
+        self._reset()
+
+
 class ArrayDock(widgets.QDockWidget):
     # These signals are controlled globally or require synchronisation
     update_cursor = Signal(float, float)
     duplicate = Signal(object)
     closed = Signal(object)
 
-    def __init__(self, array: np.ndarray, title: str, copy: int = 1) -> None:
+    def __init__(
+        self, array: np.ndarray, title: str, copy: int, can_delete: bool
+    ) -> None:
         if array.ndim != 3:
             raise ValueError("Must be a 3 dimensional array (time, y, x).")
 
@@ -34,7 +144,7 @@ class ArrayDock(widgets.QDockWidget):
             widgets.QDockWidget.DockWidgetFeature.DockWidgetMovable
             | widgets.QDockWidget.DockWidgetFeature.DockWidgetFloatable
         )
-        if not self.is_original:
+        if not self.is_original or can_delete:
             features |= widgets.QDockWidget.DockWidgetFeature.DockWidgetClosable
 
         self.setFeatures(features)
@@ -222,7 +332,6 @@ class ArrayViewerApp(widgets.QMainWindow):
         self.num_frames = arrays[0].shape[0]
         self.docks: List[ArrayDock] = []
         self.dock_instances: dict[str, int] = {}
-
         self.frame_label = widgets.QLabel()
         self._init_ui()
 
@@ -232,20 +341,23 @@ class ArrayViewerApp(widgets.QMainWindow):
         self.update_frames(0)
         self.timer = QTimer()
         self.timer.timeout.connect(self.advance_frame)
+        self.operation_dock = OperationDock(self)
+        self.addDockWidget(Qt.DockWidgetArea.TopDockWidgetArea, self.operation_dock)
+
         self.setFocusPolicy(Qt.FocusPolicy.StrongFocus)
 
-    def _validate_shape_of(self, data: np.ndarray) -> None:
+    def _validate_shape_of(self, array: np.ndarray) -> None:
         if self._enforced_shape is None:
-            self._enforced_shape = data.shape[-3:]
-        if data.shape[-3:] != self._enforced_shape:
+            self._enforced_shape = array.shape[-3:]
+        if array.shape[-3:] != self._enforced_shape:
             raise ValueError(
-                f"Data provided has shape {data.shape}, must be same as others ({self._enforced_shape})."
+                f"Array provided has shape {array.shape}, but it must be same as others ({self._enforced_shape})."
             )
 
-    def register_array(self, array: np.ndarray, title: str) -> "ArrayViewerApp":
+    def _add_array(self, array: np.ndarray, title: str, can_delete: bool) -> None:
         self._validate_shape_of(array)
-        self.dock_instances[title] = self.dock_instances.get(title, 0) + 1
-        dock = ArrayDock(array=array, title=title, copy=self.dock_instances[title])
+        self.dock_instances[title] = copy = self.dock_instances.get(title, 0) + 1
+        dock = ArrayDock(array=array, title=title, copy=copy, can_delete=can_delete)
 
         # track instance
         self.docks.append(dock)
@@ -256,6 +368,9 @@ class ArrayViewerApp(widgets.QMainWindow):
         dock.closed.connect(self._remove_dock)
 
         self.addDockWidget(Qt.DockWidgetArea.TopDockWidgetArea, dock)
+
+    def register_array(self, array: np.ndarray, title: str) -> "ArrayViewerApp":
+        self._add_array(array, title, can_delete=False)
         return self
 
     def _remove_dock(self, dock: ArrayDock) -> None:
@@ -280,10 +395,10 @@ class ArrayViewerApp(widgets.QMainWindow):
             super().keyPressEvent(event)
 
     def duplicate_dock(self, dock: ArrayDock) -> None:
-        self.register_array(
-            dock.get_array(),
-            title=dock.get_title(),
-        )
+        self._add_array(dock.get_array(), title=dock.get_title(), can_delete=False)
+
+    def get_original_docks(self) -> list[ArrayDock]:
+        return [dock for dock in self.docks if dock.is_original]
 
     def _init_ui(self) -> None:
         self.setDockNestingEnabled(True)
