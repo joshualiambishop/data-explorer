cf181bb0ac9a9794b5423b003ef88c326c5fa54c
Josh Bishop <josh.bishop@refeyn.com>
2025-05-03 14:37:06 +0100

    skeletons

--- data_explorer/app.py
+++ data_explorer/app.py
@@ -14,7 +14,7 @@ import logging
 LOGGER: Final[logging.Logger] = logging.getLogger(__name__)
 
 
-class ArrayViewerApp(widgets.QMainWindow):
+class _ArrayViewerApp(widgets.QMainWindow):
 
     num_array_changed = Signal()
 
@@ -283,10 +283,20 @@ class ArrayViewerApp(widgets.QMainWindow):
             dock.sync_crosshair(x, y)
 
 
+class FrameProvider:
+
+    def get_frame(self, index: int) -> np.ndarray: ...
+
+
+class ArrayViewerApp:
+    def __init__(self) -> None:
+        self._data: dict[str, FrameProvider] = {}
+
+
 def launch_viewer(arrays: Sequence[np.ndarray], titles: Sequence[str]) -> None:
     # reuse a running QApplication if present
     app = widgets.QApplication.instance() or widgets.QApplication(sys.argv)
-    viewer = ArrayViewerApp(arrays, titles)
+    viewer = _ArrayViewerApp(arrays, titles)
     viewer.setWindowTitle("3D Array Viewer")
     viewer.resize(1200, 800)
     viewer.show()
